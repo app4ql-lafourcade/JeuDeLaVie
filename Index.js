665f9171f0b740db4d5f8c240eab9c5877fc4e7d
Baptiste Lafourcade <blafour@a-109626.ups.u-psud.fr>
2016-05-09 16:05:38 +0200

    bien

--- Index.js
+++ Index.js
@@ -2,26 +2,11 @@
         var canvas = document.getElementById('can');
         var context = canvas.getContext('2d');
         var a = [];
-        setInterval(anime, 1000);
-       /*
+        var timer = document.getElementById('timer').value ;
+        setTimeout(anime, timer);
+			
         function anime() {
-            context.fillStyle = "green";
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            for (i=0; i<10; i++) {
-                for (j=0; j<10; j++) {
-                    if (Math.random() > 0.9) {
-                        a[i*10+j] = 1;
-                        context.fillStyle = "green";
-                    } else {
-                        a[i*10+j] = 0;
-                        context.fillStyle = "red";
-                    }
-                    context.fillRect(10*i, 10*j, 10, 10);
-                }
-            }
-        }
-       */
-        function anime() {
+			var timer = document.getElementById('timer').value ;
 			var nb = document.getElementById('nb').value ;
 			var taille = document.getElementById('taille').value ;
 			console.log("a");
@@ -39,5 +24,6 @@
                     context.fillRect(taille*i, taille*j, taille, taille);
                 }
             }
+            setTimeout(anime, timer);
         }
     }
